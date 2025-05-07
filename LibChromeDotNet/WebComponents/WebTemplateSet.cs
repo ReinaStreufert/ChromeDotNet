cedfe7ce0159bec806a90b4c1b00c64944424d1f
ReinaStreufert <49079389+ReinaStreufert@users.noreply.github.com>
2025-05-07 17:01:51 -0400

    Update WebTemplateSet.cs

--- LibChromeDotNet/WebComponents/WebTemplateSet.cs
+++ LibChromeDotNet/WebComponents/WebTemplateSet.cs
@@ -1,6 +1,7 @@
 ﻿using LibChromeDotNet.WebComponents.ILGen;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -11,6 +12,20 @@ namespace LibChromeDotNet.WebComponents
 {
     public class WebTemplateSet : IWebTemplateSet
     {
+        private static ImmutableDictionary<string, Type>? LoadedTypes;
+
+        private static ImmutableDictionary<string, Type> GetLoadedTypes()
+        {
+            if (LoadedTypes == null)
+            {
+                LoadedTypes = AppDomain.CurrentDomain.GetAssemblies()
+                    .SelectMany(a => a.GetExportedTypes())
+                    .Where(t => t.FullName != null)
+                    .ToImmutableDictionary(t => t.FullName!);
+            }
+            return LoadedTypes;
+        }
+
         private Dictionary<string, XmlDocument> _UnloadedTemplates = new Dictionary<string, XmlDocument>();
         private Dictionary<string, SubstituterInfo> _TemplateSubstituters = new Dictionary<string, SubstituterInfo>();
         private IEnumerable<ISubstituterBuilder> _SubstituterBuilders;
@@ -44,6 +59,7 @@ namespace LibChromeDotNet.WebComponents
                 return true;
             if (!_UnloadedTemplates.ContainsKey(name))
                 throw new ArgumentException($"{nameof(name)} does not refer to an included template");
+            return false;
         }
 
         public SubstituterInfo LoadTemplate(string name)
@@ -59,7 +75,44 @@ namespace LibChromeDotNet.WebComponents
 
         private SubstituterInfo BuildSubstituter(XmlDocument descriptor)
         {
+            var docElement = descriptor.DocumentElement!;
+            var templateName = docElement.GetAttribute("name");
+            if (!docElement.HasAttribute("bindingType"))
+                throw new FormatException($"Template '{templateName}' has no bindingType attribute defined");
+            var typeName = docElement.GetAttribute("bindingType");
+            if (!GetLoadedTypes().TryGetValue(typeName, out var bindingType))
+                throw new FormatException($"Template references binding type which does not exist in any loaded assembly '{typeName}'");
+            var resourceParameter = Expression.Parameter(typeof(IComponentResource));
+            var renderContextParamater = Expression.Parameter(typeof(IComponentRenderContext));
+            if (docElement.ChildNodes.OfType<XmlElement>().Count() != 1)
+                throw new FormatException($"Template tag must have only one child tag");
+            var rootBuilderTag = docElement.ChildNodes
+                .OfType<XmlElement>()
+                .First();
+            var lambdaExpression = Expression.Lambda(
+                Expression.Block(BuildLambdaBody(rootBuilderTag, resourceParameter, renderContextParamater, bindingType)),
+                renderContextParamater,
+                resourceParameter);
+            return new SubstituterInfo(bindingType, (Substituter)lambdaExpression.Compile());
+        }
 
+        private IEnumerable<Expression> BuildLambdaBody(XmlElement rootBuilderTag, ParameterExpression resourceParameter, ParameterExpression renderContextParameter, Type bindingType)
+        {
+            var thisParameter = Expression.Parameter(bindingType);
+            yield return Expression.Assign(thisParameter, Expression.Convert(resourceParameter, bindingType));
+            var documentElementParameter = Expression.Parameter(typeof(XmlElement));
+            var xmlDocumentGetter = typeof(IComponentRenderContext).GetProperty(nameof(IComponentRenderContext.Document))!
+                .GetGetMethod()!;
+            var documentElementGetter = typeof(XmlDocument).GetProperty(nameof(XmlDocument.DocumentElement))!
+                .GetGetMethod()!;
+            yield return Expression.Assign(
+                documentElementParameter, 
+                Expression.Call(Expression.Call(renderContextParameter, xmlDocumentGetter), 
+                documentElementGetter));
+            var scope = new BuilderScope(this, thisParameter, renderContextParameter, documentElementParameter);
+            var rootBuilder = scope.GetBuilderForTagName(rootBuilderTag.Name);
+            foreach (var expr in rootBuilder.GetExpressions(rootBuilderTag, scope))
+                yield return expr;
         }
 
         private class BuilderScope : ISubstituterBuilderScope
@@ -109,12 +162,36 @@ namespace LibChromeDotNet.WebComponents
 
             public ISubstituterBuilder GetBuilderForTagName(string name)
             {
-                throw new NotImplementedException();
+                return _Set._SubstituterBuilders
+                    .Where(b => b.IsMatchFor(name))
+                    .First();
             }
 
             public Substitution GetSubstitutionBinding(string memberExpr)
             {
-                throw new NotImplementedException();
+                Expression binding = _ThisBinding;
+                Expression? resourceTreeTip = binding;
+                var first = true;
+                foreach (var segment in memberExpr.Split('.'))
+                {
+                    if (first)
+                    {
+                        first = false;
+                        if (_LocalParameterScope.TryGetValue(segment, out var localExpr))
+                        {
+                            binding = localExpr;
+                            resourceTreeTip = binding.Type.IsSubclassOf(typeof(IComponentResource)) ? binding : null;
+                            break;
+                        }
+                    }
+                    var propertyGetter = binding.Type.GetProperty(segment)?.GetGetMethod();
+                    if (propertyGetter == null)
+                        throw new ArgumentException($"template references '{segment}' which is not a member of type '{binding.Type}'");
+                    binding = Expression.Call(binding, propertyGetter);
+                    if (propertyGetter.ReturnType.IsSubclassOf(typeof(IComponentRenderContext)))
+                        resourceTreeTip = binding;
+                }
+                return new Substitution(binding, resourceTreeTip);
             }
 
             public void SetLocalBinding(string name, ParameterExpression value)
@@ -132,12 +209,12 @@ namespace LibChromeDotNet.WebComponents
 
             private WebTemplateSet _Set;
 
-            public IEnumerable<Expression> GetExpressions(XmlElement substitutionPrototype, ISubstituterBuilderScope builderScope)
+            public IEnumerable<Expression> GetExpressions(XmlElement substitutionPrototype, ISubstituterBuilderScope scope)
             {
                 var substituterInfo = _Set.LoadTemplate(substitutionPrototype.Name);
                 var branchMethod = typeof(IComponentRenderContext).GetMethod(nameof(IComponentRenderContext.Branch));
                 var branchedRenderContextParameter = Expression.Parameter(typeof(IComponentRenderContext))!;
-                yield return Expression.Assign(branchedRenderContextParameter, Expression.Call(builderScope.ComponentRenderContext, branchMethod!));
+                yield return Expression.Assign(branchedRenderContextParameter, Expression.Call(scope.ComponentRenderContext, branchMethod!));
                 var resourceType = substituterInfo.ResourceType;
                 var resourceParameter = Expression.Parameter(substituterInfo.ResourceType);
                 var resourceConstructor = resourceType.GetConstructor(new Type[] { })!;
@@ -148,9 +225,11 @@ namespace LibChromeDotNet.WebComponents
                         ?? throw new ArgumentException($"template contains reference to a property which does not exist '{resourceType.Name}.{attributeNode.Name}'");
                     var setter = property.GetSetMethod()
                         ?? throw new ArgumentException($"template property '{property.DeclaringType}.{property.Name}' has no public set accessor");
-                    
+                    var propertyValue = scope.GetSubstitutionBinding(attributeNode.Value);
+                    yield return Expression.Call(resourceParameter, setter, propertyValue.ValueExpression);
                 }
-
+                var substitutorMethod = substituterInfo.Substituter.Method;
+                yield return Expression.Call(substitutorMethod, branchedRenderContextParameter, resourceParameter);
             }
 
             public bool IsMatchFor(string name) => _Set.IsTemplateIncluded(name);
